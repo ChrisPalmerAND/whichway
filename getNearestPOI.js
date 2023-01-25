362c14f43b7d846e8b2706440f680823a9580df7
kiramarston <kiramarston@gmail.com>
2023-01-25 15:35:49 +0000

    removed access token

--- getNearestPOI.js
+++ getNearestPOI.js
@@ -2,8 +2,7 @@ const axios = require('axios');
 
 const pointOfInterest = 'train';
 const coordinates = '-4.135470257448143,55.77419730873031';
-const accessToken =
-	'pk.eyJ1Ijoia21hcnN0b24iLCJhIjoiY2xkYWJxaHU3MDhxMjNvbm8ybHB2YXo2NSJ9.ZJ90vI_IkPJZQUYzglqyxQ';
+const accessToken = 'OOPS';
 
 const api = `https://api.mapbox.com/geocoding/v5/mapbox.places/${pointOfInterest}.json?type=poi&proximity=${coordinates}&access_token=${accessToken}`;
 
