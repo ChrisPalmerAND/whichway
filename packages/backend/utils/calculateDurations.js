10eccbcae9b6ca0eb831ced174294f85a39738c0
Antonio Lorusso <antonio.lorusso@cabinetoffice.gov.uk>
2023-01-27 18:20:48 +0000

    enanche if with early returns

--- packages/backend/utils/calculateDurations.js
+++ packages/backend/utils/calculateDurations.js
@@ -1,19 +1,19 @@
 export const calculateBusDuration = (distance, duration) => {
   if (distance < 5000) {
     return duration * 1.2;
-  } else if (distance >= 5000 && distance <= 10000) {
-    return duration * 1.4;
-  } else {
-    return duration * 1.66;
   }
+  if (distance >= 5000 && distance <= 10000) {
+    return duration * 1.4;
+  }
+  return duration * 1.66;
 };
 
 export const calculateTrainDuration = (distance, duration) => {
   if (distance < 5000) {
     return duration * 0.9;
-  } else if (distance >= 5000 && distance <= 10000) {
-    return duration * 0.8;
-  } else {
-    return duration * 0.7;
   }
+  if (distance >= 5000 && distance <= 10000) {
+    return duration * 0.8;
+  }
+  return duration * 0.7;
 };
