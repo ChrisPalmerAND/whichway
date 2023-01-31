62dff91f8e6ee8490af39364a7f565653761a437
Antonio Lorusso <antonio.lorusso@cabinetoffice.gov.uk>
2023-01-31 12:15:58 +0000

    add sidebar

---
+++ packages/frontend/src/components/AppBarWrapper.js
@@ -0,0 +1,47 @@
+import Info from '@mui/icons-material/Info';
+import MuiAppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import 'leaflet-draw/dist/leaflet.draw.css';
+import 'leaflet/dist/leaflet.css';
+import React from 'react';
+/* eslint-disable react/prop-types */
+export const AppBarWrapper = ({ handleDrawerOpen, open, drawerWidth }) => {
+    const AppBar = styled(MuiAppBar, {
+        shouldForwardProp: (prop) => prop !== 'open',
+    })(({ theme, open }) => ({
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        ...(open && {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: `${drawerWidth}px`,
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        }),
+    }));
+
+    return (
+        <AppBar position="fixed" open={open}>
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                >
+                    <Info />
+                </IconButton>
+                <Typography variant="h6" noWrap component="div">
+                    Whichway?
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
