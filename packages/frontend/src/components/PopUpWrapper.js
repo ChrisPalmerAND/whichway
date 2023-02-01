c4deced4985cec0bd62c5fa5e305284b2431cacc
kiramarston <kiramarston@gmail.com>
2023-02-01 17:07:57 +0000

    Added a working popup and some icons!

--- packages/frontend/src/components/PopUpWrapper.js
+++ packages/frontend/src/components/PopUpWrapper.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Popup } from 'react-leaflet';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export const PopUpWrapper = ({ details, setActiveProperty }) => {
     return (
         <Popup
@@ -9,18 +11,36 @@ export const PopUpWrapper = ({ details, setActiveProperty }) => {
             }}
         >
             <div>
-                <p>Rent: {details.rent}</p>
                 <p>
-                    Cycling:
-                    {` ${details.cycling.distance} metre / ${details.cycling.duration} seconds`}
+                    <FontAwesomeIcon icon="fa-solid fa-house" /> Rent: £{details.rent} pcm
                 </p>
                 <p>
-                    driving:
-                    {` ${details.driving.distance} metre / ${details.driving.duration} seconds`}
+                    <FontAwesomeIcon icon="fa-solid fa-car-side" /> Driving:
+                    {` ${details.driving.distance} metres / ${details.driving.duration} seconds`}
                 </p>
                 <p>
-                    train:
-                    {` ${details.train} seconds`}
+                    <FontAwesomeIcon icon="fa-solid fa-bicycle" /> Cycling:
+                    {` ${details.cycling.distance} metres / ${details.cycling.duration} seconds`}
+                </p>
+                <p>
+                    <FontAwesomeIcon icon="fa-solid fa-person-walking" /> Walking:
+                    {` ${details.driving.distance} metres / ${details.driving.duration} seconds`}
+                </p>
+                <p>
+                    <FontAwesomeIcon icon="fa-solid fa-train-subway" /> Train:
+                    {` ${details.train} metres`}
+                </p>
+                <p>
+                    <FontAwesomeIcon icon="fa-solid fa-train-subway" /> Nearest Train Station:
+                    {` ${details.nearestTrainStation.address} metres`}
+                </p>
+                <p>
+                    <FontAwesomeIcon icon="fa-solid fa-bus" /> Bus:
+                    {` ${details.train} metres`}
+                </p>
+                <p>
+                    <FontAwesomeIcon icon="fa-solid fa-bus" /> Nearest Bus Stop:
+                    {` ${details.nearestBusStop.address} metres`}
                 </p>
             </div>
         </Popup>
